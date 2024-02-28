2bd6f236776238b902466218e687c68b704ffebf
Roni003 <ronikazmirci003@gmail.com>
2024-02-28 15:53:14 +0000

    added empty data into mid section

--- src/components/MidSection.jsx
+++ src/components/MidSection.jsx
@@ -1,9 +1,14 @@
 import "./MidSection.css"
 
 const MidSection = () => {
-  return (<div className="mid">
-    Middle
-  </div>);
+  return (
+    <div className="mid">
+      <h1>Mile end</h1>
+      <div>Icon</div>
+      <h2>19℃</h2>
+      <h3>Light Rain</h3>
+      <h3>Feels like 17℃</h3>
+    </div>);
 }
 
 export default MidSection;
