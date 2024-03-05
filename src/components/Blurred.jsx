eeace9b2add71a74d06786b76713c301a26cde14
tafsi <ec22621@qmul.ac.uk>
2024-03-05 18:08:03 +0000

    added scrollable, alignment is messed up though

--- src/components/Blurred.jsx
+++ src/components/Blurred.jsx
@@ -1,12 +1,21 @@
 import TFLButton from "./TFLButton";
-import "./Blurred.css"
+import "./Blurred.css";
 
 const Blurred = () => {
   return (
     <div className="blurred-container">
-
       <div className="hourly-forecast">
-        Hourly forecast
+        <h1>Hourly forecast</h1>
+        <div className="forecast-container">
+          <div className="forecast-item">12:00 Sunny (Temp)</div>
+          <div className="forecast-item">13:00 Rain (Temp)</div>
+          <div className="forecast-item">14:00 Cloudy (Temp)</div>
+          <div className="forecast-item">15:00 Cloudy (Temp)</div>
+          <div className="forecast-item">16:00 Cloudy (Temp)</div>
+          <div className="forecast-item">17:00 Cloudy (Temp)</div>
+          <div className="forecast-item">18:00 Cloudy (Temp)</div>
+          <div className="forecast-item">19:00 Cloudy (Temp)</div>
+        </div>
       </div>
 
       <div className="lines">
@@ -19,8 +28,8 @@ const Blurred = () => {
           <TFLButton color={"gray"} />
         </div>
       </div>
-
-    </div>);
-}
+    </div>
+  );
+};
 
 export default Blurred;
