479bf03076ab141139673d0001985f6d20336528
tafsi <ec22621@qmul.ac.uk>
2024-03-09 17:36:16 +0000

    Added sidebar template. almost done needs a few more touches

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./TopSection.css"
+import "./TopSection.css";
+import Sidebar from "./Sidebar";
 
 const TopSection = () => {
-  return (<div className="top">
-    <div>...</div>
-    <div><Link to={"/locations"}>+</Link></div>
-  </div>);
-}
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleLabelClick = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="top">
+      <div className="menu">
+        <input type="checkbox" name="checkbox" id="check" />
+        <label htmlFor="check" onClick={handleLabelClick}>
+          ...
+        </label>
+      </div>
+
+      <div>
+        <Link to={"/locations"}>+</Link>
+      </div>
+      {isSidebarOpen && <Sidebar />}
+    </div>
+  );
+};
 
 export default TopSection;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,16 @@
-import "./Home.css"
+import "./Home.css";
 import TopSection from "../components/TopSection";
 import MidSection from "../components/MidSection";
 import Blurred from "../components/Blurred";
 
 const Home = () => {
-  return (<div className="home">
-    <TopSection />
-    <MidSection />
-    <Blurred />
-  </div>);
-}
+  return (
+    <div className="home">
+      <TopSection />
+      <MidSection />
+      <Blurred />
+    </div>
+  );
+};
 
 export default Home;
