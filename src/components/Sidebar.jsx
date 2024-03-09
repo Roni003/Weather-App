0534ba26845953e31d4a1223d5e4c9f71a212507
tafsi <ec22621@qmul.ac.uk>
2024-03-09 18:57:26 +0000

    Added files.. should be working now

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
