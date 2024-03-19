e8914848edf60329a103f5bfd1b239b984b9b7ae
Roni003 <ronikazmirci003@gmail.com>
2024-03-19 23:11:38 +0000

    removed unused stuff

--- src/routes/Locations.jsx
+++ src/routes/Locations.jsx
@@ -5,6 +5,8 @@ import SearchBar from "../components/SearchBar";
 import {
   getLocationFromLocalStorage,
   setLocationInLocalStorage,
+  getWeatherInfo,
+  getHourlyWeatherInfo,
 } from "../functions/ow.js";
 import { useEffect, useState } from "react";
 
@@ -53,52 +55,6 @@ const Locations = () => {
   const addLocation = (index) => {
     setLocationInLocalStorage(index);
   };
-
-  async function getWeatherInfo(location) {
-    const { lat, lon } = await getCoordsFromLocation(location);
-    const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${OW_API_KEY}`;
-
-    console.log("Fetching weather data for", location);
-
-    return fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        return {
-          location: data.name,
-          temperature: data.main,
-          wind: data.wind,
-          weather: data.weather,
-          timezone: data.timezone,
-        };
-      })
-      .catch((err) => console.log(err));
-  }
-
-  async function getHourlyWeatherInfo(location) {
-    const { lat, lon } = await getCoordsFromLocation(location);
-    const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?units=metric&lat=${lat}&lon=${lon}&appid=${OW_API_KEY}`;
-
-    console.log("Fetching Hourly weather data for", location);
-
-    return fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        return { data: data.list };
-      })
-      .catch((err) => console.log(err));
-  }
-
-  function getCoordsFromLocation(location) {
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${OW_API_KEY}`;
-
-    return fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        data = data[0];
-        return { lat: data.lat, lon: data.lon };
-      })
-      .catch((err) => console.log(err));
-  }
 
   return (
     <>
